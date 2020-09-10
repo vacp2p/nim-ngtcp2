01eda2479961960f55c5dfbe47ef95a6cca429b1
Mark Spanbroek <mark@spanbroek.net>
2020-09-10 17:09:14 +0200

    Import sockets code instead of copying it

--- prelude.nim
+++ prelude.nim
@@ -1,8 +1,11 @@
-# C include directories
-
 import os
 import strformat
+import ngtcp2/sockets
 
+# Socket definitions
+export sockets
+
+# C include directories
 const root = currentSourcePath.parentDir
 const sourceInclude = root/"sources"/"lib"/"includes"
 const buildInclude = root/"build"/"lib"/"includes"
