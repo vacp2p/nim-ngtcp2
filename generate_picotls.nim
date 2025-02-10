710b98bd8d4873f7f2c83cdceb8eebbcea8210a2
Richard Ramos <info@richardramos.me>
2025-02-10 17:07:37 -0400

    refactor: use single file

--- generate_picotls.nim
+++
@@ -1,8 +0,0 @@
-import futhark, strutils
-from os import parentDir, `/`
-
-importc:
-    outputPath currentSourcePath.parentDir / "picotls.nim"
-    path currentSourcePath.parentDir/"libs"/"picotls"/"include"
-    "picotls.h"
-
--- generate_picotls_openssl.nim
+++
@@ -1,8 +0,0 @@
-import futhark, strutils
-from os import parentDir, `/`
-
-importc:
-    outputPath currentSourcePath.parentDir / "picotls_openssl.nim"
-    path currentSourcePath.parentDir/"libs"/"picotls"/"include"
-    "picotls/openssl.h"
-
