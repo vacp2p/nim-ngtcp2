7b50e9721f888eb1cb99ea026377e9fb1575b52f
Richard Ramos <info@richardramos.me>
2025-07-09 13:27:40 -0400

    test

--- build_libs.sh
+++ build_libs.sh
@@ -2,6 +2,10 @@
 root=$(dirname "$0")
 sources=${root}/libs
 
+
+cmake -S . -B build
+
+
 # build aws-lc
 tmpdir=$(mktemp -d)
 cmake -S ./libs/aws-lc -B "$tmpdir" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DFIPS=OFF -DBUILD_TOOL=OFF -DDISABLE_GO=ON
